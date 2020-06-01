5b3388662afba0e84573b9cefbd038cb2ab15cf7
haku <1173044198@qq.com>
2020-06-01 13:11:00 +0800

    chage file NSObject+RQKit

--- RQKit/Category/Foundation/NSObject+RQKit.m
+++ RQKit/Category/Foundation/NSObject+RQKit.m
@@ -12,57 +12,6 @@
 
 @implementation NSObject (RQKit)
 
-/*
- NSInvocation is much slower than objc_msgSend()...
- Do not use it if you have performance issues.
- */
-
-#define INIT_INV(_last_arg_, _return_) \
-NSMethodSignature * sig = [self methodSignatureForSelector:sel]; \
-if (!sig) { [self doesNotRecognizeSelector:sel]; return _return_; } \
-NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig]; \
-if (!inv) { [self doesNotRecognizeSelector:sel]; return _return_; } \
-[inv setTarget:self]; \
-[inv setSelector:sel]; \
-va_list args; \
-va_start(args, _last_arg_); \
-[NSObject setInv:inv withSig:sig andArgs:args]; \
-va_end(args);
-
-- (id)performSelectorWithArgs:(SEL)sel, ...{
-    INIT_INV(sel, nil);
-    [inv invoke];
-    return [NSObject getReturnFromInv:inv withSig:sig];
-}
-
-- (void)performSelectorWithArgs:(SEL)sel afterDelay:(NSTimeInterval)delay, ...{
-    INIT_INV(delay, );
-    [inv retainArguments];
-    [inv performSelector:@selector(invoke) withObject:nil afterDelay:delay];
-}
-
-- (id)performSelectorWithArgsOnMainThread:(SEL)sel waitUntilDone:(BOOL)wait, ...{
-    INIT_INV(wait, nil);
-    if (!wait) [inv retainArguments];
-    [inv performSelectorOnMainThread:@selector(invoke) withObject:nil waitUntilDone:wait];
-    return wait ? [NSObject getReturnFromInv:inv withSig:sig] : nil;
-}
-
-- (id)performSelectorWithArgs:(SEL)sel onThread:(NSThread *)thr waitUntilDone:(BOOL)wait, ...{
-    INIT_INV(wait, nil);
-    if (!wait) [inv retainArguments];
-    [inv performSelector:@selector(invoke) onThread:thr withObject:nil waitUntilDone:wait];
-    return wait ? [NSObject getReturnFromInv:inv withSig:sig] : nil;
-}
-
-- (void)performSelectorWithArgsInBackground:(SEL)sel, ...{
-    INIT_INV(sel, );
-    [inv retainArguments];
-    [inv performSelectorInBackground:@selector(invoke) withObject:nil];
-}
-
-#undef INIT_INV
-
 + (id)getReturnFromInv:(NSInvocation *)inv withSig:(NSMethodSignature *)sig {
     NSUInteger length = [sig methodReturnLength];
     if (length == 0) return nil;
